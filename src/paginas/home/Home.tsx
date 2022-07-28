e66b21164ef8a3e899e641302b002181077ce630
Manoela <manoelamelo24@gmail.com>
2022-07-28 14:18:19 -0300

    iniciando projeto

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+return (
+    <>
+        <h1 className= "titulo">Home</h1>
+        <img src="https://cpplimeira.com.br/wp-content/uploads/2015/11/23.11.2015-logo-Lei-da-inclus%C3%A3o-PNG.png" alt="imagem sobre a inclusão" className='img'/>
+    </>
+);
+
+}
+
+export default Home;
