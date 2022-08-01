707adb9a43841adc31d00854226e91b4420620cd
Manoela <manoelamelo24@gmail.com>
2022-08-01 14:43:46 -0300

    react2

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/estaticos/footer/Footer';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import './App.css';
 
 function App() {
     return (
-      <>
-      <Navbar/>
-      <Home />
-      <Footer />
-        
+        <Router>
+            <Navbar />
+            <div style={{ minHeight: '100vh' }}>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/home" element={<Home />} />
 
-    </>
+                    {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+                </Routes>
+            </div>
+            <Footer />
+        </Router>
+
     );
-    
+
 }
 
 export default App;
