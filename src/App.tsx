3ff57db2a80f377bafcb951034e444c44edc0111
Manoela <manoelamelo24@gmail.com>
2022-08-12 14:10:19 -0300

    Atividade React 5,6,7,9,10

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,58 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/estaticos/footer/Footer';
 import Navbar from './components/estaticos/navbar/Navbar';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import ListaTema from './components/temas/listatema/ListaTema';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import './App.css';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import { Provider } from 'react-redux';
+import store from './store/store';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 
 function App() {
     return (
+        <Provider store={store}>
+
         <Router>
             <Navbar />
             <div style={{ minHeight: '100vh' }}>
                 <Routes>
                     <Route path="/" element={<Login />} />
+
                     <Route path="/login" element={<Login />} />
+
                     <Route path="/home" element={<Home />} />
 
-                    {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+                    <Route path="/cadastroUsuario" element={<CadastroUsuario />} />
+
+                    <Route path="/temas" element={<ListaTema />} />
+
+                    <Route path="/posts" element={<ListaPostagem />} />
+                    
+                    <Route path="/formularioPostagem" element={<CadastroPost />} />
+
+                    <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+                    <Route path="/formularioTema" element={<CadastroTema />} />
+
+                    <Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+                    <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+                    <Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+
                 </Routes>
             </div>
             <Footer />
         </Router>
+        
+        </Provider>
 
     );
 
--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,16 +1,61 @@
-import React from "react";
-import { Grid, Button, TextField, Typography } from "@material-ui/core";
-import { Box } from "@mui/material";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import { Grid, Typography, TextField, Button } from '@material-ui/core';
+import {Box } from '@mui/system';
+import { Link, useNavigate } from 'react-router-dom';
+import { login } from '../../services/Service';
+import UserLogin from '../../models/UserLogin';
 import './Login.css';
+import { useDispatch } from 'react-redux';
+import { addToken } from '../../store/tokens/actions';
 
 function Login() {
+    let navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [token, setToken] = useState('');
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            usuario: '',
+            senha: '',
+            token: ''
+        }
+        )
+
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
+        }
+
+            useEffect(()=>{
+                if(token != ''){
+                    dispatch(addToken(token));
+                    navigate('/home')
+                }
+            }, [token])
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+            e.preventDefault();
+            try{
+                await login(`/usuarios/logar`, userLogin, setToken)
+
+                alert('Usuário logado com sucesso!');
+            }catch(error){
+                alert('Dados do usuário inconsistentes. Erro ao logar!');
+            }
+        }
+
+
+
+
     return (
         <Grid container direction="row" justifyContent="center" alignItems="center">
             <Grid alignItems="center" xs={6}>
                 <Box paddingX={20}>
                     <form>
-                        <Typography variant="h3" gutterBottom color='textPrimary' component='h3' align='center' className="texto1">Entar </Typography>
+                        <Typography variant="h3" gutterBottom color='textPrimary' component='h3' align='center' className="texto1">Entrar </Typography>
                         <TextField id='usuario' label='usuario' variant="outlined" name='usuario' margin="normal" fullWidth />
                         <TextField id='senha' label='senha' variant="outlined" name='senha' margin="normal" type='password' fullWidth />
                         <Box marginTop={2} textAlign='center'>
@@ -26,7 +71,9 @@ function Login() {
                         <Box marginRight={1}>
                             <Typography variant="subtitle1" gutterBottom align="center" className="texto1">Não tem uma conta?</Typography>
                         </Box>
+                        <Link to="/cadastrousuario">
                         <Typography variant="subtitle1" gutterBottom align="center" className="texto1">Cadastre-se</Typography>
+                        </Link>
                     </Box>
                 </Box>
             </Grid>
